8f04bdb70dec6f9fdbb60dee81e8682dadb184c1
Christian Guirreri <christian@guirreri.com>
2020-02-10 11:15:59 -0500

    fix: Incorrect path to custom widget

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,5 +1,5 @@
 import CMS, { init } from 'netlify-cms'
 console.log(CMS)
-import { Control, Preview } from '../src/admin-custom-widget'
+import { Control, Preview } from '../admin-custom-widget'
 CMS.registerWidget('test', Control, Preview)
 init()
